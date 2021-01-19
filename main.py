348ca9979cd53506b85f9edf26fae16d5d00404e
gleb89 <37249444+gleb89@users.noreply.github.com>
2021-01-19 22:26:10 +0300

    wq

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:3000",
-    "https://zapic.online/"
+    "https://zapic.online/",
+    "http://zapic.online/"
 ]
 
 
